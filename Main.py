7196b07cb9d6d3af1e3152007940bdc1b5eac6ff
dscrt <danzinho.autentico@gmail.com>
2025-08-17 17:19:45 -0300

    Add files via upload
    Aprendendo a utilizar Branches, colisões e me comunicar pelo terminal através de interações do jogo.

--- Main.py
+++ Main.py
@@ -6,8 +6,8 @@ from sys import exit
 pygame.init()
 largura = 640
 altura = 480
-x = largura / 2
-y = 0
+x = largura / 2  # tanto a 'x' quanto a 'y' controla o movimento do persongame
+y = altura / 2
 
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Jogo da velha")
@@ -21,9 +21,29 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             exit()
-    pygame.draw.rect(tela, (255, 0, 0), (x, y, 50, 62))
-    if y >= altura:
-        y = 0
+        if event.type== KEYDOWN:
+            if event.key == K_a:
+                x = x - 20
+            if event.key == K_d:
+                x = x + 20
+            if event.key == K_w:
+                y = y - 20
+            if event.key == K_s:
+                y = y + 20
 
-    y = y + 1
+    if pygame.key.get_pressed()[K_a]:
+        x = x - 2
+    if pygame.key.get_pressed()[K_d]:
+        x = x + 2
+    if pygame.key.get_pressed()[K_s]:
+        y = y + 2
+    if pygame.key.get_pressed()[K_w]:
+        y = y - 2
+
+    ret_ver = pygame.draw.rect(tela, (255, 0, 0), (x, y, 50, 62))
+    ret_azul = pygame.draw.rect(tela, (0,0,255), (150, 120, 40, 50))
+
+    if ret_ver.colliderect(ret_azul): #Alteração para que ocorra colisão com nome no terminal... aprendendo ainda mais!
+        print("comeu")
+
     pygame.display.update()
