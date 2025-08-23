25161b2a9bfde005ec31e56cd14f84e2196deb0b
dscrt <danzinho.autentico@gmail.com>
2025-08-22 22:07:18 -0300

    Fiz algumas alterações.
    Implementação de um segundo personagem (quadrado azul) no jogo. Adicionada lógica de colisão: quando o jogador encosta no quadrado azul, ele é reposicionado aleatoriamente na tela usando randint.

--- Main.py
+++ Main.py
@@ -1,16 +1,20 @@
 import pygame
 from pygame.locals import *
 from sys import exit
+from random import randint
 
 #incializando o pygame
 pygame.init()
 largura = 640
 altura = 480
-x = largura / 2
-y = 0
+x = largura / 2  # tanto a 'x' quanto a 'y' controla o movimento do persongame
+y = altura / 2
+
+x_blu = randint(40, 600)
+y_blu = randint(50, 430)
 
 tela = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption("Jogo da velha")
+pygame.display.set_caption("Jogo")
 relogio = pygame.time.Clock()
 
 #loop infinito
@@ -21,9 +25,30 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             exit()
-    pygame.draw.rect(tela, (255, 0, 0), (x, y, 50, 62))
-    if y >= altura: # fiz algumas alterações aqui, para mover o 'Player'.
-        y = 0
+        if event.type== KEYDOWN:
+            if event.key == K_a:
+                x = x - 20
+            if event.key == K_d:
+                x = x + 20
+            if event.key == K_w:
+                y = y - 20
+            if event.key == K_s:
+                y = y + 20
 
-    y = y + 1
+    if pygame.key.get_pressed()[K_a]:
+        x = x - 3
+    if pygame.key.get_pressed()[K_d]:
+        x = x + 3
+    if pygame.key.get_pressed()[K_s]:
+        y = y + 3
+    if pygame.key.get_pressed()[K_w]:
+        y = y - 3
+
+    ret_ver = pygame.draw.rect(tela, (255, 0, 0), (x, y, 50, 62))
+    ret_azul = pygame.draw.rect(tela, (0,0,255), (x_blu, y_blu, 40, 50))
+
+    if ret_ver.colliderect(ret_azul): #Alteração para que ocorra colisão com nome no terminal... aprendendo ainda mais!
+        x_blu = randint(40, 600)
+        y_blu = randint(50, 430)
+
     pygame.display.update()
